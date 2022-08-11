822678c54bda362932e7f943c64cf41c7c12131a
jxe1 <108363329+jxe1@users.noreply.github.com>
2022-08-11 17:18:07 +0300

    Add files via upload

---
+++ _analog_circute_1.ino
@@ -0,0 +1,22 @@
+int sensorPin = 0;
+ 
+void setup()
+{
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+ 
+ int reading = analogRead(sensorPin);
+
+ float voltage = reading * 4.68;
+ voltage /= 1024.0;
+ 
+ 
+ float temperatureC = (voltage - 0.5) * 100;
+ Serial.print(temperatureC);
+ Serial.println(" degrees C");
+ 
+ delay(1000);
+}
